97d5b281ae800dec810fdc179550266b87c7fb3d
Liberbell <liberbell@gmail.com>
2019-10-19 14:02:51 +0900

    functions3

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 def main():
     name = input('Please input your name: ')
     time = input('Please input the time of day (morninig/afternoon/evening): ')
-    print('Good' + time + ',' + name + '!')
+    print('Good' + time + ' , ' + name + '!')
 
 if __name__ == '__main__':
     main()

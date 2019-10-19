75eb8837ee1a93b2a9b642c844605442f0edbb41
Liberbell <liberbell@gmail.com>
2019-10-19 14:10:18 +0900

    prints6

--- prints.py
+++ prints.py
@@ -3,4 +3,7 @@ print('Hello world!')
 print(2000 + 19)
 
 name = input('What is your name: ')
-color = input('What is your favorite color?')
+color = input('What is your favorite color? ')
+
+print(name)
+print(color)

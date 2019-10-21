b0b11a74a47ce680893d61b079423ee2005ded21
Liberbell <liberbell@gmail.com>
2019-10-21 12:58:27 +0900

    cal8

--- cal.py
+++ cal.py
@@ -13,4 +13,20 @@ print(number)
 
 for i in range(100):
     number = random.randint(1, 100)
-    print(number)
+    state1 = 0
+    state2 = 0
+    state3 = 0
+    state4 = 0
+    state5 = 0
+    if number == 1:
+        state1 += 1
+    elif number == 2:
+        state2 += 1
+    elif number == 3:
+        state3 += 1
+    elif number == 4:
+        state4 += 1
+    else:
+        state5 +=1
+
+print('1 is: ', state1)

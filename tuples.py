b00a84e346324f24eeff5abc870bf19a3fa7892c
Liberbell <liberbell@gmail.com>
2019-10-23 07:49:07 +0900

    tuples3

--- tuples.py
+++ tuples.py
@@ -1,2 +1,4 @@
 child1_birth = ('Julia', 'Lucile Packard Children`s Hospital', 'Stanford', 'California', 'United States', '06/20/2019', '11:20')
-child2_birth = ('Julia', 'Lucile Packard Children`s Hospital', 'Stanford', 'California', 'United States', '06/20/2019', '11:20')
+child2_birth = ('Austin', 'Washington Hospital', 'Fremont', 'California', 'United States', '08/10/1981', '16:40')
+
+print(type(child1_birth))

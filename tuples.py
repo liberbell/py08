d6919d9df8837b332398ce35f053b68d3a1d8d36
Liberbell <liberbell@gmail.com>
2019-10-23 07:47:44 +0900

    tuples2

---
+++ tuples.py
@@ -0,0 +1,2 @@
+child1_birth = ('Julia', 'Lucile Packard Children`s Hospital', 'Stanford', 'California', 'United States', '06/20/2019', '11:20')
+child2_birth = ('Julia', 'Lucile Packard Children`s Hospital', 'Stanford', 'California', 'United States', '06/20/2019', '11:20')

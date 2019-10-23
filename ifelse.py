d92d55e3b38f2d4d654f749713ca70b58bc254d9
Liberbell <liberbell@gmail.com>
2019-10-24 07:52:24 +0900

    ifelse2

---
+++ ifelse.py
@@ -0,0 +1,7 @@
+raining = input('Is it raining yes/no: ')
+unbrella = input('Do you have an unbrella yes/no: ')
+
+if raining == 'yes' and unbrella == 'yes':
+    print('Don`t forget your unbrella when you go out.')
+elif raining == 'yes' and unbrella == 'no':
+    print('Wear a waterproof jacket when yuo go out.')

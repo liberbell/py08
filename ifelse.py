7482f6172b6261c43a1289697da97fbc6625c0ae
Liberbell <liberbell@gmail.com>
2019-10-25 07:39:30 +0900

    ifelse1

--- ifelse.py
+++ ifelse.py
@@ -5,3 +5,14 @@ if raining == 'yes' and unbrella == 'yes':
     print('Don`t forget your unbrella when you go out.')
 elif raining == 'yes' and unbrella == 'no':
     print('Wear a waterproof jacket when yuo go out.')
+
+x = input('Enter a number here: ')
+x = float(x)
+if x < 2:
+    print('The number is less than two.')
+elif x < 6:
+    print('The number is less than six.')
+elif x < 8:
+    print('The number is less than eight.')
+elif x < 10:
+    print('The number is less than ten.')
